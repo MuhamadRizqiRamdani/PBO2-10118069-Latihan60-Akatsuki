c9ec1583d5775062c008fd0ed0e0675a1a6de6df
Muhamad Rizqi Ramdani <55569266+MuhamadRizqiRamdani@users.noreply.github.com>
2019-11-07 21:42:21 +0700

    Add files via upload

---
+++ src/main/java/com/mycompany/pbo2/latihan60/akatsuki/Test.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.pbo2.latihan60.akatsuki;
+import java.util.Scanner;
+/**
+ *
+ * @author asus
+ * NAMA     : Muhamad Rizqi Ramdani
+ * KELAS    : IF-2
+ * NIM      : 10118069
+ * Deskripsi Program : Akatsuki
+ */
+public class Test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Akatsuki akatsuki = new Akatsuki();
+        
+        System.out.println("===Tokoh Akatsuki Berdasarkan Desa/Asal===");
+        akatsuki.tampilDaftarDesa();
+        akatsuki.setDesa(sc.nextInt());
+        
+        System.out.println("");
+        
+        akatsuki.tampilAnggotaDesa(akatsuki.getDesa());
+    }
+}
